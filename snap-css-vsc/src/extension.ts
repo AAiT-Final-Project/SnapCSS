09f345dbc9b85bc394e8062005cdbe6503643c24
yiseboge <Ethiopia1!>
2021-08-15 23:48:22 +0300

    [vsc] Initialize VSC Extension project

---
+++ snap-css-vsc/src/extension.ts
@@ -0,0 +1,31 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import SnapCss = require('snappy-css');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "snap-css-vsc" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('snap-css-vsc.optimize', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Welcome to Optimization with Snap CSS!');
+		const snap = new SnapCss();
+		const optimizers = snap.getOptimizers("a");
+		let css = snap.getCSS("Trial CSS code Goes Here");
+		optimizers.forEach((optimizer) => (css = optimizer.optimize(css)));
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
