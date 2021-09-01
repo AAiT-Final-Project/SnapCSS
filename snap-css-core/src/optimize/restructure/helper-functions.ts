115d9956c53b9506395536d5d4a084ddecb6c593
YeabsiraTesfaye <45359568+YeabsiraTesfaye@users.noreply.github.com>
2021-09-01 12:16:25 +0300

    helper functions for restructure

---
+++ snap-css-core/src/optimize/restructure/helper-functions.ts
@@ -0,0 +1,69 @@
+export function reverseString(str: string) {
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+export function intersection_destructive(a: any, b: any) {
+    var result = [];
+    while (a.length > 0 && b.length > 0) {
+        if (a[0] < b[0]) { a.shift(); }
+        else if (a[0] > b[0]) { b.shift(); }
+        else /* they're equal */ {
+            result.push(a.shift());
+            b.shift();
+        }
+    }
+    return result;
+}
+
+export function differences(a: any, b: any) {
+    var dif: any = [];
+    for (var key in a) { //In a and not in b
+        if (!b[key.trim()]) {
+            dif.push(key.trim())
+        }
+    }
+    for (key in b) { //in b and not in a
+        if (!a[key.trim()]) {
+            dif.push(key.trim())
+        }
+    }
+    for (var key in a) {
+        if (b[key.trim()] && a[key.trim()] != b[key.trim()]) {
+            dif.push(key.trim())
+        }
+    }
+    for (key in b) {
+        if (a[key.trim()] && a[key.trim()] != b[key.trim()]) {
+            dif.push(key.trim())
+        }
+    }
+    return dif;
+}
+export function similar(a: any, b: any) {
+    let sim: any = [];
+    for (var key in a) {
+        if (b[key.trim()] && a[key.trim()] == b[key.trim()]) {
+            sim[key.trim()] = a[key.trim()];
+        }
+    }
+    for (key in b) {
+        if (a[key.trim()] && a[key.trim()] == b[key.trim()]) {
+            sim[key.trim()] = b[key.trim()];
+        }
+    }
+    return sim;
+}
+export function toUniqueArray(a: any) {
+    var newArr: any = [];
+    for (var i = 0; i < a.length; i++) {
+        if (newArr.indexOf(a[i]) === -1) {
+            newArr.push(a[i]);
+        }
+    }
+    return newArr;
+}
+
