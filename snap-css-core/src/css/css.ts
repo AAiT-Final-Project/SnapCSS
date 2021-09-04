bf6e7ab44991849e5293fcac4fddfd902c7b81b2
yiseboge <Ethiopia1!>
2021-09-04 22:45:02 +0300

    [core] Implement expanding shorthand properties before parsing it

--- snap-css-core/src/css/css.ts
+++ snap-css-core/src/css/css.ts
@@ -1,9 +1,11 @@
 import RuleSet from './rule-set'
 import * as postcss from 'postcss'
+const expand = require('postcss-shorthand-expand')
 
 export default class CSS {
   static fromString(string: string) {
-    const root = postcss.parse(string).root()
+    const css = require('postcss')([expand()]).process(string)
+    const root = postcss.parse(css).root()
     const result = new CSS([RuleSet.fromAST(root)])
     root.walkAtRules(atRule => {
       result.ruleSets.push(RuleSet.fromAST(atRule))
