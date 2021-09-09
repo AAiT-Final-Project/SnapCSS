5854bc49b1936c7ca26c8fef44e7171a872bd27d
Yisehak Bogale <yisehakb@gmail.com>
2021-09-10 00:39:46 +0300

    [core] Modify the JSON exporting functionality

--- snap-css-core/src/css/declaration.ts
+++ snap-css-core/src/css/declaration.ts
@@ -3,13 +3,14 @@ const expander = require('shortcss')
 
 export default class Declaration {
   private static processValue(value: string) {
-    const separated = value.split(/(\d+)/).filter(x => x.length)
+    const separated = value.split(/([0-9.\-+]+)/).filter(x => x.length)
     const options = [
-      ['^[-+]?\\d+?[a-zA-Z]*$', separated[0], 'DIMENSION', separated[1]],
+      ['^[-+]?\\d*\\.?\\d+[a-zA-Z]+$', separated[0], 'DIMENSION', separated[1]],
       ['^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3}|[a-fA-F0-9]{8}|[a-fA-F0-9]{4})$', value, 'HASH', ''],
       ['^[-+]?\\d+$', value, 'INTEGER', ''],
-      ['^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$', value, 'NUMBER', ''],
-      ['\\d+(?:\\.\\d+)?%', separated[0], 'PERCENTAGE', separated[1]],
+      ['^[-+]?\\d*\\.?\\d+$', value, 'NUMBER', ''],
+      ['^[\\w\\-]+\\(.*\\)$', value, 'FUNCTION', ''],
+      ['^[-+]?\\d*\\.?\\d+%$', separated[0], 'PERCENTAGE', separated[1]],
     ]
     for (let i = 0; i < options.length; i++)
       if (value.match(new RegExp(options[i][0])))
@@ -34,8 +35,8 @@ export default class Declaration {
     public property: string,
     public value: string,
     public type: string,
-    public unit: string,
-    public important: boolean
+    public unit = '',
+    public important = false
   ) {
   }
 
