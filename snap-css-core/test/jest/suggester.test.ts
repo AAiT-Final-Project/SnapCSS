7e76887363c5943879b81acf4f80691573e15ad3
TumsaUmata <tumsaumata2020@gmail.com>
2021-09-27 20:16:56 +0300

    [Core] Add Integration Testing

--- snap-css-core/test/jest/suggester.test.ts
+++ snap-css-core/test/jest/suggester.test.ts
@@ -4,7 +4,7 @@ import {testData, expectedResult} from './test-cases'
 describe('UC05 - Suggest Class Names', () => {
   test('TC_Suggest_01: Check that suggestions are added to blocks of code', async () => {
     let css = snap.getCSS(testData.TC_Suggest_001)
-    css = await snap.optimize(css, 'a')
+    css = await snap.optimize(css, 's')
     expect(css.toString().split('\n').join('')).toContain(expectedResult.TC_Suggest_001)
   })
   test('TC_Suggest_02: Check the response of the system when it can not make suggestions', async () => {
